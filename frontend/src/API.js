bba874c63a16b1256fe680d882d8c0ad1efee39f
Pierre-Alexis Maerten <pierre-alexis.maerten@hetic.net>
2023-03-26 22:21:43 +0200

    fin quatrième partie

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,13 +1,29 @@
 import axios from "axios";
 
 export const getTodos = async () => {
-  //TO_MODIFY
+  try {
+    const response = await axios.get("/todos");
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const createTodo = async (formData) => {
-  //TO_MODIFY
+  try {
+    const response = await axios.post("http://localhost:4001/todos", formData);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const deleteTodo = async (todoId) => {
-  //TO_MODIFY
+  try {
+    const response = await axios.delete(`http://localhost:4001/todos/todos/${todoId}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error deleting todo");
+  }
 };
